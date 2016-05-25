1d6a7ae93ab49ef602c357f42b31ee4b93a08ad6
Michael Bang <michael@katnegermis.dk>
2016-05-25 20:28:53 +0200

    Add recursive flag.

--- cmdpool.go
+++ cmdpool.go
@@ -19,18 +19,22 @@ import (
 var dir string
 var cmd string
 var workers int
+var recursive bool
 
 const fileNameSymb = "{filename}"
 const filePathSymb = "{filepath}"
 
 func parseFlags() {
-	_dir := flag.String("dir", "", "Path to files to run command on.")
-	_cmd := flag.String("cmd", "", fmt.Sprintf("Command to run, %s and %s will be substituded.", filePathSymb, fileNameSymb))
+	_dir := flag.String("dir", "", "Dir containing files to run 'cmd' on.")
+	_cmd := flag.String("cmd", "", fmt.Sprintf("Command to run, %s and %s will be substituded for files in 'dir'.", filePathSymb, fileNameSymb))
 	_workers := flag.Int("workers", runtime.NumCPU(), "Number of go routines to spawn cmd on.")
+	_recursive := flag.Bool("recursive", false, "Whether 'dir' should be searched recursively.")
 
 	flag.Parse()
 
 	workers = *_workers
+	dir = *_dir
+	recursive = *_recursive
 
 	if len(*_cmd) == 0 {
 		flag.Usage()
@@ -39,24 +43,33 @@ func parseFlags() {
 	}
 	cmd = *_cmd
 
-	dir = *_dir
 }
 
 func getFiles() []string {
 	files := make([]string, 0, 512)
-	entries, err := ioutil.ReadDir(dir)
-	if err != nil {
-		log.Fatal(err)
+
+	var _getFiles func(string) []string
+	_getFiles = func(dir string) []string {
+		entries, err := ioutil.ReadDir(dir)
+		if err != nil {
+			log.Println(err)
+		}
+		for _, entry := range entries {
+			if entry.IsDir() {
+				_getFiles(path.Join(dir, entry.Name()))
+			} else {
+				files = append(files, path.Join(dir, entry.Name()))
+			}
+		}
+		return files
 	}
-	for _, entry := range entries {
-		files = append(files, entry.Name())
-	}
-	return files
+
+	return _getFiles(dir)
 }
 
-func getCmdArr(fileName string) []string {
-	_cmd := strings.Replace(cmd, fileNameSymb, fileName, -1)
-	_cmd = strings.Replace(_cmd, filePathSymb, path.Join(dir, fileName), -1)
+func getCmdArr(filePath string) []string {
+	_cmd := strings.Replace(cmd, fileNameSymb, path.Base(filePath), -1)
+	_cmd = strings.Replace(_cmd, filePathSymb, path.Join(dir, filePath), -1)
 	cmdArr, err := shellwords.Parse(fmt.Sprintf("sh -c \"%s\"", _cmd))
 	if err != nil {
 		log.Fatal(err)
